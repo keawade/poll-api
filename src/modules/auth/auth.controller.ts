9a19428d1ccf9eb509195c5ce5a39665c20da424
Keith Wade <keawade@gmail.com>
2017-05-12 23:03:52 -0500

    Working towards auth

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,16 +1,21 @@
-import { Controller, Get, Post, Response, Param, HttpStatus } from 'nest.js';
+import { Controller, Get, Post, Response, Request, Body, Param, HttpStatus } from 'nest.js';
 import { AuthService } from './auth.service';
+import * as jwt from 'jsonwebtoken';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) { }
 
-  @Get()
-  public testResponse( @Response() res) {
+  @Post('register')
+  public registerUser( @Response() res, @Request() req, @Body('user') user) {
     try {
-      res.status(HttpStatus.OK).json('It worked!');
+      if (!user) {
+        return res.status(HttpStatus.BAD_REQUEST).json({ error: 'No user provided.' });
+      }
+      console.log(`[auth.controller] POST /auth/register`, user);
+      return res.status(HttpStatus.OK).json({ user });
     } catch (err) {
-      res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ error: err })
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ error: err })
     }
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,35 @@ import * as express from 'express';
 import * as morgan from 'morgan';
 import * as cors from 'cors';
 
+import * as mongoose from 'mongoose';
+
+import * as passport from 'passport';
+import * as passportGithub from 'passport-github';
+import * as bodyParser from 'body-parser';
+
 import { NestFactory } from 'nest.js';
 import { ApplicationModule } from './modules/app.module';
 
+// mongoose.connect(process.env.DB_ADDRESS || 'mongodb://localhost/poll-api');
+// mongoose.connection.on('connected', () => {
+//   console.log(`[server] database connection established`);
+// });
+
 // Initialize express with middleware
 const expressInstance = express();
+passport.use(new passportGithub.Strategy({
+  clientID: '',
+  clientSecret: '',
+  callbackURL: '//localhost:3000/auth/github/callback',
+}, (accessToken, refreshToken, profile, done) => {
+  // Get and return user;
+  return done(null, profile);
+}));
+
+expressInstance.use(bodyParser.json());
+
+expressInstance.use(passport.initialize());
+
 expressInstance.use(morgan('dev'));
 expressInstance.use(cors());
 
@@ -14,4 +38,4 @@ const app = NestFactory.create(ApplicationModule, expressInstance);
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`[server.ts] listening on port ${port}.`));
+app.listen(port, () => console.log(`[server] listening on port ${port}.`));
