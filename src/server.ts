381feb35ab682468daca6ec0840a387087f0a4f2
Keith Wade <keawade@gmail.com>
2017-05-14 16:02:56 -0500

    Remove github auth stuff

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,6 @@ import * as cors from 'cors';
 import * as express from 'express';
 import * as mongoose from 'mongoose';
 import * as morgan from 'morgan';
-import * as passport from 'passport';
-import * as passportGithub from 'passport-github';
 
 import { NestFactory } from 'nest.js';
 import { ApplicationModule } from './modules/app.module';
@@ -17,19 +15,7 @@ mongoose.connection.on('connected', () => {
 
 // Initialize express with middleware
 const expressInstance = express();
-passport.use(new passportGithub.Strategy({
-  callbackURL: '//localhost:3000/auth/github/callback',
-  clientID: 'aa4289ed9ab4df542ba5',
-  clientSecret: '46ab834537afbc89df72e176c5f0978cec91a25b',
-}, (accessToken, refreshToken, profile, done) => {
-  // Get and return user;
-  return done(null, profile);
-}));
-
 expressInstance.use(bodyParser.json());
-
-expressInstance.use(passport.initialize());
-
 expressInstance.use(morgan('dev'));
 expressInstance.use(cors());
 
